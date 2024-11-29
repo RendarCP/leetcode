d0287b37c2944df7b3cd66bf0b65b45291012147
RendarCB <32770277+RendarCP@users.noreply.github.com>
2024-11-29 23:49:19 +0900

    Time: 104 ms (5.75%), Space: 56.6 MB (5.37%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -1,8 +1,24 @@
 function mergeAlternately(word1: string, word2: string): string {
- let result = '';
-  for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-    if (i < word1.length) result += word1[i];
-    if (i < word2.length) result += word2[i];
-  }
-  return result;
+    let tempArr = []
+    let length = 0
+    let i = 0;
+    const splitword1 = word1.split('');
+    const splitword2 = word2.split('');
+    if(splitword1.length >= splitword2.length) { 
+        length = splitword1.length;
+    }
+    
+    if(splitword2.length > splitword1.length) {
+        length = splitword2.length;
+    }
+    console.log('lenfth', length);
+    console.log('splotword1',splitword1);
+    console.log('splitword2', splitword2);
+
+    for(i=0; i< length; i++) {
+        tempArr.push(splitword1[i])
+        tempArr.push(splitword2[i])
+    }
+    console.log('tempArr',tempArr)
+    return tempArr.join("")
 };
