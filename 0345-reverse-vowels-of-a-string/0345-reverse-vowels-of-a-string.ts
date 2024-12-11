ac3aaba992ecc3c0ca8cee9c7ff295196d1f4f37
RendarCB <32770277+RendarCP@users.noreply.github.com>
2024-12-11 23:43:45 +0900

    Time: 8 ms (67.44%), Space: 55.5 MB (87.54%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -5,7 +5,6 @@ function reverseVowels(s: string): string {
     let right = arr.length - 1;
 
     while (left < right) {
-        // Move left pointer until a vowel is found or pointers cross
         while (left < right && !vowels.has(arr[left])) {
             left++;
         }
@@ -13,7 +12,6 @@ function reverseVowels(s: string): string {
         while (left < right && !vowels.has(arr[right])) {
             right--;
         }
-        // If both are vowels, swap them
         if (left < right) {
             let temp = arr[left];
             arr[left] = arr[right];
